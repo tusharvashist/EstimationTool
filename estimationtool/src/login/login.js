bd47620a5d94d500d2cdcb9cb2984611d7f67fa8
Bhanu Maniktahla <Bhanu.maniktahla@pyramidconsultinginc.com>
2021-09-16 11:43:06 +0530

    login,auth and localstorage implementation FED-74-84-86

--- estimationtool/src/login/login.js
+++ estimationtool/src/login/login.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
+import loginSer from "./login.service";
+import authSer from "../shared/service/auth";
 
 import Button from '@material-ui/core/Button';
 import FormControl from '@material-ui/core/FormControl';
@@ -12,26 +14,32 @@ export default function Login(props) {
     const [state, setState] = React.useState({
         email: "",
         pass: "",
-      });
-      
+    });
+
     const handleEmail = (e) => {
         let val = e.target.value;
-        setState({...state, email:val});
+        setState({ ...state, email: val });
     };
 
     const handlePass = (e) => {
         let val = e.target.value;
-        setState({...state, pass:val});
+        setState({ ...state, pass: val });
     };
 
-    const redirectDashbord =()=>{
+    const redirectDashbord = () => {
         props.LoginFun();
         let url = "/home"
-       history.push(url)  
+        history.push(url)
     }
+    useEffect(() => {
+        if (localStorage.getItem('user-info')) {
+            history.push("/home");
+        }
+    }, [])
 
-    const isVlaidLogin = ()=>{
-        if(state.email === 'admin' && state.pass === "admin"){
+
+    const isVlaidLogin = () => {
+        if (state.email === 'admin' && state.pass === "admin") {
             // console.log("valid user")
             redirectDashbord()
             return true
@@ -39,34 +47,43 @@ export default function Login(props) {
         // console.log("Not valid user")
         return false
     }
-    
-    const handleLogin = function(e){
+
+
+    const handleLogin = function (e) {
         e.preventDefault();
         isVlaidLogin()
+        loginSer.login(state).then((res) => {
+            console.log(res);
+            authSer.login(res);
+            //    authSer.logout(res);    
+        }).catch((error) => {
+            console.log(error.message);
+        })
     }
+
     return (
-        <Grid container className="h-100 login-wrp"  direction="row" >
+        <Grid container className="h-100 login-wrp" direction="row" >
             <Grid item xs={6} className="bg-img" >
                 <Grid container className="bg-mask" justifyContent="center" alignItems="center">
-                    <div className="promo-box"> 
-                        <h1 className="title"> Estimation Tool </h1> 
+                    <div className="promo-box">
+                        <h1 className="title"> Estimation Tool </h1>
                         <p className="sub-title">The Scalable Path Project estimator. A tool that's flexible enough to help you estimate costs.</p>
                     </div>
                 </Grid>
-               
+
             </Grid>
             <Grid item xs={6}>
                 <Grid container justifyContent="center" alignItems="center" className="h-100" direction="column">
                     <h1 className="login-title"> Login</h1>
                     <form onSubmit={handleLogin}>
-                            <FormControl>
-                                <TextField value={state.email} placeholder="Email"  onChange={handleEmail} id="email"  size="small" variant="outlined" />
-                            </FormControl>
-                            <FormControl>
+                        <FormControl>
+                            <TextField value={state.email} placeholder="Email" onChange={handleEmail} id="email" size="small" variant="outlined" />
+                        </FormControl>
+                        <FormControl>
                             <TextField value={state.pass} id="pass" type="password" placeholder="password" onChange={handlePass} size="small" variant="outlined" />
-                            </FormControl>
+                        </FormControl>
 
-                            <Button variant="contained" color="primary"  type="submit" className="w-100"> Login </Button>
+                        <Button variant="contained" color="primary" type="submit" className="w-100"> Login </Button>
                     </form>
                 </Grid>
             </Grid>
--- estimationtool/src/shared/service/auth.js
+++ estimationtool/src/shared/service/auth.js
@@ -1,17 +1,29 @@
 import axios from "axios";
 
+const Auth = {
+    login: (userData) => {
+        let info = {
+            firstName: "jitu",
+            lastName: "jahagirdar",
+            email: "jitu@gmail.com",
+            mobileNumber: "9800000000",
+            address: "Street 9, 4Th avenue, San Fransisco, USA",
+            img: "https://preview.keenthemes.com/metronic-v4/theme/assets/pages/media/profile/profile_user.jpg",
+            role: "super",
+            token: "12345"
+        };
+        localStorage.setItem('auth', JSON.stringify(info));
 
-const login = (user)=>{
-    let info = { 'one': 1, 'two': 2, 'three': 3 };
-    localStorage.setItem('auth', JSON.stringify(info));
-
+    },
+    logout : (user) => {
+        // let getauth = localStorage.getItem('testObject');
+        // console.log('retrieved Object: ', JSON.parse(getauth));
+        localStorage.removeItem("auth")
+    }
 }
 
-const logout = (user)=>{
-    // let getauth = localStorage.getItem('testObject');
-    // console.log('retrieved Object: ', JSON.parse(getauth));
-    localStorage.removeItem("auth")
-}
 
 
-export default Auth = {  login, logout }
+
+
+export default Auth 
