ff8d9ce05f3538feccf4547d7d555556b84a6227
JituPy <90328260+JituPy@users.noreply.github.com>
2021-09-27 10:10:30 +0530

    env setup

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -2,10 +2,26 @@ const mongoose = require("mongoose");
 
 module.exports = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/estimationtool", {useNewUrlParser: true });
-    console.log("database connected");
-  } catch (err) {
-    console.log("database connected", err);
+    const url = process.env.DB_URL || 'mongodb://localhost:27017/estimationtool';
+    let localMsz = ()=>{ console.log("Your MongoDB Database connected to local successfully ..! (:")}
+    let prodctionMsz = ()=>{console.log("Your MongoDB Database connected to Prodction successfully ..(:")}
+
+    if(process.env.NODE_ENV === 'production'){
+      return await mongoose.connect(url, {useNewUrlParser: true }).then(()=>{
+        prodctionMsz()
+      }).catch((err)=>{
+        console.log("Error: MoboDB Database Not connected :( ", err);
+      });
+    }
+
+    return await mongoose.connect(url, {useNewUrlParser: true }).then(()=>{
+      localMsz()
+      }).catch((err)=>{
+        console.log("Error: MoboDB Database Not connected :( ", err);
+      });
+
+  }catch (err) {
+    console.log("MoboDB Database Not connected :( ", err);
     throw new Error(err);
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,11 @@ const cors = require("cors");
 const dbconnection = require("./database/connection");
 const swaggerUi = require("swagger-ui-express"); 
 const yaml = require("yamljs"); 
-const swaggerDocument = yaml.load('./swagger.yaml'); 
+const swaggerDocument = yaml.load('./swagger.yaml');
+const productionURL="https://estimationtoolapi.azurewebsites.net/";
 const app = express();
+const PORT = process.env.PORT || 5252;
+let envName = "";
 dotEnv.config();
 
 //do connection
@@ -32,25 +35,35 @@ app.use("/api/v1/project", require("./routes/project.route"));
 
 //----------- API Documentation
 if(process.env.NODE_ENV != "production"){
-    app.get("/api-docs",swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  envName = "Locally"
+  app.get("/api-docs",swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+} 
+if(process.env.NODE_ENV === "production"){
+  envName = "Production"
 }
 
 app.get("/", (req, res) => {
   res.send(
-    `<style> body { padding: 0; margin: 0; font-family: Arial, sans-serif; } .wrp { background: #f3f3f3; height: 100%; } .brand{ color: #fff; font-size: 13px; } .main-content { height:calc(100% - 50px); width: 100%; display: flex; align-items: center; justify-content: center; flex-direction: column; } .title { font-size: 14px; font-style: normal; color: #77bf77; } .top-nav { background-color: #5459ff; height: 50px; box-shadow: 1px 1px 1px #d0cccc; padding: 0 20px; display: flex; align-items: center; } .server-port{ color: #3ac720; font-size: 21px; transition: all 1s; animation: 700ms blink infinite; } .server-msz{ } @keyframes blink { 10% { color: transparent; } 50% { color: transparent; } } </style> <div class='wrp'> <div class='top-nav'><h6 class='brand'>Estimation Tool Server</h6></div> <div class='main-content'> <p class='server-msz'>Estimation Tool Node Server Running on port : <span class='server-port'>${PORT}</span></p> <img src='https://images.squarespace-cdn.com/content/v1/58d20c79725e25b221549193/1524135572127-95TS4RSNLNS7D5DWORN9/nodejs.png'> </div> </div>`
+    `<style> body { padding: 0; margin: 0; font-family: Arial, sans-serif; } .wrp { background: #f3f3f3; height: 100%; } .brand{ color: #fff; font-size: 13px; } .main-content { height:calc(100% - 50px); width: 100%; display: flex; align-items: center; justify-content: center; flex-direction: column; } .title { font-size: 14px; font-style: normal; color: #77bf77; } .top-nav { background-color: #5459ff; height: 50px; box-shadow: 1px 1px 1px #d0cccc; padding: 0 20px; display: flex; align-items: center; } .server-port{ color: #3ac720; font-size: 21px; transition: all 1s; animation: 700ms blink infinite; } .server-msz{ } @keyframes blink { 10% { color: transparent; } 50% { color: transparent; } } </style> <div class='wrp'> <div class='top-nav'><h6 class='brand'>Estimation Tool Server</h6></div> <div class='main-content'> <p class='server-msz'>Node API Server Running <b> ${envName}</b> port : <span class='server-port'>${PORT}</span></p> <img src='https://images.squarespace-cdn.com/content/v1/58d20c79725e25b221549193/1524135572127-95TS4RSNLNS7D5DWORN9/nodejs.png'> </div> </div>`
   );
 });
 
-const PORT = process.env.PORT || 5252;
+
 
 app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
+    let prodctionMsz=()=>{
+      console.log("Node API Application Your Running On Production env...!", `${productionURL}`)
+    }
+    let localMsz =()=>{
+      console.log("Node API Application Running locally",`http://localhost:${PORT}`)
+    }
+  if(process.env.NODE_ENV === "production"){
+    return prodctionMsz()
+  }
+  return localMsz()
 });
 
 //--------- Error handel  middleware
-
-
-
 app.use((err, req, res, next) => {
   res.status(500).send({
     status: 500,
