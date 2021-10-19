7c469d6396a03de87f7c52d39029cc792867cb59
Gupta Rahul <grahul@pyramidconsultinginc.com>
2021-10-19 12:39:31 +0530

    Created refrance og clint ,project,Estimation. Updated clin and project

--- backend/apiSchma/jioClientSchema.js
+++ backend/apiSchma/jioClientSchema.js
@@ -1,15 +1,19 @@
 const Joi = require("joi");
 module.exports.createClientSchema = Joi.object({
-    clientName:Joi.string().required(),
-    website:Joi.string().required(),
-    description:Joi.string().required()
+    clientName: Joi.string().required(),
+    website: Joi.string().required(),
+    description: Joi.string().required(),
+    isDeleted: Joi.boolean().required()
 })
-module.exports.getallClients = Joi.object({ 
-    skip:Joi.string(),
-    limit:Joi.string()
+module.exports.getClientById = Joi.object({
+    id: Joi.string().required()
 })
-module.exports.clientUpdateSchema = Joi.object({ 
-    clientName:Joi.string(),
-    website:Joi.string(),
-    description:Joi.string()
+module.exports.getallClients = Joi.object({
+    skip: Joi.string(),
+    limit: Joi.string()
+})
+module.exports.clientUpdateSchema = Joi.object({
+    clientName: Joi.string(),
+    website: Joi.string(),
+    description: Joi.string()
 })
--- backend/database/models/projectModel.js
+++ backend/database/models/projectModel.js
@@ -1,11 +1,29 @@
+const { boolean } = require("joi");
 const mongoose = require("mongoose");
+const { Schema } = require('mongoose');
+
 const projectSchema = new mongoose.Schema({
-    projectName:String,
-    projectDescription:String
-},{
-    timestamps:true,
-    toObject:{
-        transform: function(doc,ret,option){
+    projectName: String,
+    projectDescription: String,
+    domain: String,
+    isDeleted: Boolean,
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    client: {
+        type: Schema.Types.ObjectId,
+        ref: 'client',
+        required: true
+    },
+    estimates: [{
+        type: Schema.Types.ObjectId,
+        ref: 'estimation'
+    }]
+}, {
+    timestamps: true,
+    toObject: {
+        transform: function (doc, ret, option) {
             ret.id = ret._id;
             delete ret._id;
             delete ret.__v;
