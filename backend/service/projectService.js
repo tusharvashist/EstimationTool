037229d6c49b3dc1c4f25558a008dfed7f56d65f
vishal Verma <vishalflame07@gmail.com>
2021-10-21 15:10:27 +0530

    Project Detail APi Fixes

--- backend/service/projectService.js
+++ backend/service/projectService.js
@@ -37,9 +37,7 @@ module.exports.getProjectById = async ({ id }) => {
     if (!mongoose.Types.ObjectId(id)) {
       throw new Error(constant.projectMessage.INVALID_ID)
     }
-    let project = await Project.findById(id)
-    .populate('client')
-    .populate('estimates')
+    let project = await Project.findById(id).populate('client').populate('estimates')
     if (!project) {
       throw new Error(constant.projectMessage.PROJECT_NOT_FOUND)
     }
