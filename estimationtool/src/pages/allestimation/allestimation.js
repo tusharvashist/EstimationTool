68c69081e8d4712e869bb6ff2c50c64828046345
Tushar Vashist <Tusharva@pyramidconsultinginc.com>
2021-10-21 12:28:44 +0530

    table ui changes

--- estimationtool/src/pages/allestimation/allestimation.js
+++ estimationtool/src/pages/allestimation/allestimation.js
@@ -1,54 +1,94 @@
 import MaterialTable from "material-table";
-import React, { useState, useEffect  } from "react";
+import React, { useState, useEffect } from "react";
 import "./allestimation.css";
 import AllestimationSer from "./allestimation.service";
-import Link from '@material-ui/core/Link';
+import Link from "@material-ui/core/Link";
+import { fontSize, style } from "@material-ui/system";
+import { createMuiTheme } from "@material-ui/core";
 function Home() {
-    const [tableData, setTableData] = useState([]);
-    const [editRow, setEditRow] = useState({});
-    useEffect(() => {
-      AllestimationSer.getAllEstimation().then((res)=>{
+  const [tableData, setTableData] = useState([]);
+  const [editRow, setEditRow] = useState({});
+  useEffect(() => {
+    AllestimationSer.getAllEstimation()
+      .then((res) => {
         let dataResponce = res.data.body;
-        setTableData([...tableData,...dataResponce])
-      }).catch((err)=>{
-        console.log("estimation error",err)
+        setTableData([...tableData, ...dataResponce]);
       })
-    },[]);
-    const [isOpenDailog, setIsOpenDailog] = useState(false);
-    const clientDetailsUrl = "/clientdetails/"+"614f3c6790a42ca5a74bebf6";
-    //const projectDetailsUrl = "/projectdetails/"+"614f3c6790a42ca5a74bebf6"+"/"+"614fefd74d9da71851f36df4";
-    // render:(rowData)=>{ return (<Link  href={projectDetailsUrl}> { rowData.projectName}</Link>)} 
-    const columns = [
-      { title: "Estimation Name", field: "estimationName", sorting: false },
-      { title: "Estimation Description", field: "estimationDescription" },
-      { title: "Estimation Type", field: "estimationType" },
-      { title: "Client Name", field: "clientName", render:(rowData)=>{ return (<Link  href={"/clientdetails"+"/"+rowData.id}> {rowData.clientName}</Link>)}},
-      { title: "Project Name", field: "projectName"},
-      { title: "Last update", field: "lastupdate", type:'date'},
-    ];
-    const openfn = ()=>{
-      setIsOpenDailog(true)
-    }
+      .catch((err) => {
+        console.log("estimation error", err);
+      });
+  }, []);
+  const [isOpenDailog, setIsOpenDailog] = useState(false);
+  const clientDetailsUrl = "/clientdetails/" + "614f3c6790a42ca5a74bebf6";
+  //const projectDetailsUrl = "/projectdetails/"+"614f3c6790a42ca5a74bebf6"+"/"+"614fefd74d9da71851f36df4";
+  // render:(rowData)=>{ return (<Link  href={projectDetailsUrl}> { rowData.projectName}</Link>)}
+  const columns = [
+    {
+      title: "Estimation Name",
+      field: "estimationName",
+      sorting: false,
+    },
+    {
+      title: "Estimation Description",
+      field: "estimationDescription",
+      width: "15%",
+    },
+    {
+      title: "Estimation Type",
+      field: "estimationType",
+    },
+    {
+      title: "Client Name",
+      field: "clientName",
+      render: (rowData) => {
+        return (
+          <Link href={"/clientdetails" + "/" + rowData.id}>
+            {" "}
+            {rowData.clientName}
+          </Link>
+        );
+      },
+    },
+    {
+      title: "Project Name",
+      field: "projectName",
+    },
+    {
+      title: "Last update",
+      field: "lastupdate",
+      type: "date",
+    },
+  ];
+  const openfn = () => {
+    setIsOpenDailog(true);
+  };
 
-    const closefn = ()=>{
-      setIsOpenDailog(false)
-    }
+  const closefn = () => {
+    setIsOpenDailog(false);
+  };
 
   return (
     <div className="wrap">
-       <MaterialTable
-      columns={columns}
-      options={{
-        actionsColumnIndex:-1,
-        sorting: true,
-        search: false,
-        filtering: false,
-        pageSize:5,
-        paging: false,
-      }}
-      data={tableData}
-      title="Recent Estimation"
-    />
+      <MaterialTable
+        columns={columns}
+        options={{
+          actionsColumnIndex: -1,
+          sorting: true,
+          search: false,
+          filtering: false,
+          pageSize: 5,
+          paging: false,
+          headerStyle: {
+            backgroundColor: "#1d1660",
+            color: "#fff",
+            fontWeight: "bold",
+            fontSize: "0.9rem",
+          },
+        }}
+        data={tableData}
+        title="Recent Estimation"
+        style={{ fontSize: "0.9rem" }}
+      />
     </div>
   );
 }
--- estimationtool/src/shared/layout/breadcrum/breadcrum.js
+++ estimationtool/src/shared/layout/breadcrum/breadcrum.js
@@ -1,45 +1,48 @@
-import React, {useState,useEffect} from 'react';
-import Breadcrumbs from '@material-ui/core/Breadcrumbs';
-import Link from '@material-ui/core/Link';
-import HomeIcon from '@material-ui/icons/Home';
-import Grid from '@material-ui/core/Grid';
-import { useLocation } from 'react-router-dom';
-import './breadcrum.css';
+import React, { useState, useEffect } from "react";
+import Breadcrumbs from "@material-ui/core/Breadcrumbs";
+import Link from "@material-ui/core/Link";
+import HomeIcon from "@material-ui/icons/Home";
+import Grid from "@material-ui/core/Grid";
+import { useLocation } from "react-router-dom";
+import "./breadcrum.css";
 
 export default function Breadcrum() {
-const location = useLocation();
-const [breadcrumbs, setBreadcrumbs] = useState({
-  path:"",
-  title:""
-});
-const routeList = [
-  {path:"estimation", title:"Estimation"},
-  {path:"allclient", title:"All Client"},
-  {path:"project", title:"Project"},
-  {path:"clientdetails", title:"Client Detail"},
-  {path:"projectdetails", title:"Project Detail"}
-]
+  const location = useLocation();
+  const [breadcrumbs, setBreadcrumbs] = useState({
+    path: "",
+    title: "",
+  });
+  const routeList = [
+    { path: "estimation", title: "Estimation" },
+    { path: "allclient", title: "All Client" },
+    { path: "project", title: "Project" },
+    { path: "clientdetails", title: "Client Detail" },
+    { path: "projectdetails", title: "Project Detail" },
+  ];
 
+  useEffect(() => {
+    let currentPath = location.pathname.split("/")[1];
+    getBreadcrumbsTitle(currentPath);
+  }, [location]);
 
-useEffect(() => {
-  let currentPath =  location.pathname.split("/")[1];
-  getBreadcrumbsTitle(currentPath)
-}, [location])
+  let getBreadcrumbsTitle = (currentPath) => {
+    let getbreadcrumb = routeList.find((itm, idx, arr) => {
+      return itm.path === currentPath;
+    });
+    setBreadcrumbs({ ...getbreadcrumb });
 
-let getBreadcrumbsTitle = (currentPath)=>{
-  let getbreadcrumb = routeList.find((itm,idx,arr)=>{
-    return itm.path === currentPath
-  })
-  setBreadcrumbs({...getbreadcrumb})
- 
-  console.log("currentPath>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", breadcrumbs)
-}
+    console.log("currentPath>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", breadcrumbs);
+  };
 
-    return (
-      <Grid container  alignItems="center"  className="breadcrumb-wrp">
-          <Breadcrumbs aria-label="breadcrumb" className="breadcrumb-item">
-             <p> {breadcrumbs.title} </p> 
-          </Breadcrumbs>
-      </Grid>
-      );
+  return (
+    <Grid container alignItems="center" className="breadcrumb-wrp">
+      <Breadcrumbs
+        aria-label="breadcrumb"
+        className="breadcrumb-item"
+        separator="/"
+      >
+        <p> {breadcrumbs.title} </p>
+      </Breadcrumbs>
+    </Grid>
+  );
 }
