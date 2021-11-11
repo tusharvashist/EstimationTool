b8ebc12184fea33b18096ae4cad4301d43c9a63d
tusharpyramidconsultinginc <93036983+tusharpyramidconsultinginc@users.noreply.github.com>
2021-11-11 19:45:23 +0530

    FIX: routing on finish after check

--- estimationtool/src/pages/estimationCreation/EstimationCreation.js
+++ estimationtool/src/pages/estimationCreation/EstimationCreation.js
@@ -19,6 +19,7 @@ import { useSelector, useDispatch } from "react-redux";
 import estimationServices from "../allestimation/allestimation.service";
 import "./EstimationCreation.css";
 import Snackbar from "../../shared/layout/snackbar/Snackbar";
+import { useHistory } from "react-router";
 
 const steps = ["Basic Detail", "Effort Attributes", "Calculated Attributes"];
 
@@ -26,6 +27,7 @@ const EstimationCreation = (props) => {
   console.log(props);
   const basicDetailRedux = useSelector((state) => state.basicDetail);
   const dispatch = useDispatch();
+  const history = useHistory();
   const effortAttributeSave = useSelector((state) => state.effortAttribute);
   const calcAttributeSave = useSelector((state) => state.calcAttribute);
 
@@ -45,9 +47,21 @@ const EstimationCreation = (props) => {
     setEstimationIdFinish(p);
   };
 
+  const finishLocation = {
+    pathname:
+      "/All-Clients/" +
+      clientInfo.clientName +
+      "/" +
+      projecttInfo.projectName +
+      "/Estimation-Detail",
+    state: {
+      estId: estimationIdFinish,
+    },
+  };
+
   const handleClose = () => {
-    setOpen({})
-  }
+    setOpen({});
+  };
   const childRef = useRef();
   // const secondChildRef = useRef();
 
@@ -131,47 +145,30 @@ const EstimationCreation = (props) => {
       });
   };
 
-  const handleNext = () => {
-    let newSkipped = skipped;
-    if (isStepSkipped(activeStep)) {
-      newSkipped = new Set(newSkipped.values());
-      newSkipped.delete(activeStep);
-    }
-    //TODO: handle edit basic detail API & error
-    if (activeStep == 0) {
-      handleBasicDetailSaveUpdate();
-    } else if (activeStep == 1) {
-      handleSaveEffortAttribute();
-    } else if (activeStep == 2) {
-      handleSaveCalcAttribute();
-    } else {
-      //setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    }
-    // setSkipped(newSkipped);
-    return ;
-   
-  };
-
   const handleSaveCalcAttribute = () => {
     if (calcAttributeSave.data.length !== 0) {
       createSaveCalctAttribute(getCalcAttributeRequestPayload());
-     console.log("thirdstep validation",calcAttributeSave.data)
+      // console.log("thirdstep validation", calcAttributeSave.data);
     } else {
-      setOpen({ open: true, severity: 'error', message: "Select Atleast One checkbox" });
-
+      setOpen({
+        open: true,
+        severity: "error",
+        message: "Select Atleast One checkbox",
+      });
     }
   };
 
   const handleSaveEffortAttribute = () => {
     if (effortAttributeSave.data.length !== 0) {
-
       createSaveEffortAttribute(getEffortAttributeRequestPayload());
-     
-    }else {
+    } else {
       // childRef.current.showError("Please fill all mandatory fields");
 
-      setOpen({ open: true, severity: 'error', message: "Please select atleast one checkbox" });
-
+      setOpen({
+        open: true,
+        severity: "error",
+        message: "Please select atleast one checkbox",
+      });
     }
   };
 
@@ -183,7 +180,7 @@ const EstimationCreation = (props) => {
       .then((res) => {
         let dataResponce = res.data.body;
         setActiveStep((prevActiveStep) => prevActiveStep + 1);
-        handleFinish()
+        history.push(finishLocation);
       })
       .catch((err) => {
         // console.log("save estimation header detail error : ", err);
@@ -196,7 +193,7 @@ const EstimationCreation = (props) => {
       .saveEffortAttribute(reqData)
       .then((res) => {
         let dataResponce = res.data.body;
-        setEstimationHeaderId(dataResponce._id);  
+        setEstimationHeaderId(dataResponce._id);
         //TODO:// show response and move next step
         setActiveStep((prevActiveStep) => prevActiveStep + 1);
       })
@@ -257,6 +254,26 @@ const EstimationCreation = (props) => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
 
+  const handleNext = () => {
+    let newSkipped = skipped;
+    if (isStepSkipped(activeStep)) {
+      newSkipped = new Set(newSkipped.values());
+      newSkipped.delete(activeStep);
+    }
+    //TODO: handle edit basic detail API & error
+    if (activeStep == 0) {
+      handleBasicDetailSaveUpdate();
+    } else if (activeStep == 1) {
+      handleSaveEffortAttribute();
+    }
+    // else if (activeStep == 2) {
+    // } else {
+    //   //setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    // }
+    // setSkipped(newSkipped);
+    return;
+  };
+
   const handleSkip = () => {
     if (!isStepOptional(activeStep)) {
       // You probably want to guard against something like this,
@@ -276,31 +293,12 @@ const EstimationCreation = (props) => {
     setActiveStep(0);
   };
 
-const handleFinish = () => {
-return
-  (
-    <Link
-      className="finish-link"
-      to={{
-        pathname:
-          "/All-Clients/" +
-          clientInfo.clientName +
-          "/" +
-          projecttInfo.projectName +
-          "/Estimation-Detail",
-        state: {
-          estId: estimationIdFinish,
-        },
-      }}
-    >
-      {" "}
-      Finish
-    </Link>
-  )
-}
+  const handleFinish = () => {
+    handleSaveCalcAttribute();
+  };
 
   console.log(estimationHeaderId, estimationIdFinish);
-  const { message, severity, open } = isOpen || {}
+  const { message, severity, open } = isOpen || {};
 
   return (
     <BorderedContainer>
@@ -435,11 +433,11 @@ return
                   </Button>
                 )}
 
-                <Button onClick={handleNext}>
-                  {activeStep === steps.length - 1 ? handleFinish : (
-                    "Next"
-                  )}
-                </Button>
+                {activeStep === steps.length - 1 ? (
+                  <Button onClick={handleFinish}> Finish</Button>
+                ) : (
+                  <Button onClick={handleNext}>Next</Button>
+                )}
               </Box>
             </React.Fragment>
           </>
@@ -453,7 +451,6 @@ return
           onClose={handleClose}
           message={message}
         />
-
       )}
     </BorderedContainer>
   );
