6e11088ddb85b94ed66b74bef60f00a8e62cf301
tusharpyramidconsultinginc <93036983+tusharpyramidconsultinginc@users.noreply.github.com>
2021-11-15 19:56:24 +0530

    added nav bar selected effect

--- estimationtool/src/shared/layout/sidebarv2/sidebar.js
+++ estimationtool/src/shared/layout/sidebarv2/sidebar.js
@@ -4,47 +4,67 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import HomeIcon from "@material-ui/icons/Home";
 import AssignmentIndIcon from "@material-ui/icons/AssignmentInd";
-import FolderIcon from "@material-ui/icons/Folder";
 import "./sidebar.css";
-import { Link } from "react-router-dom";
 import { withRouter } from "react-router";
 
 const Sidebarv2 = (props) => {
   const { history } = props;
 
+  const dashboardClick = () => {
+    const listitem = document.getElementsByClassName("listitem");
+    const listArr = Array.from(listitem);
+    listArr.map((el) => el.classList.remove("active"));
+
+    const dashboradbutton = document.getElementById("dashboard");
+    dashboradbutton.classList.add("active");
+
+    history.push("/Recent-Estimations");
+  };
+
+  const clientClick = () => {
+    const listitem = document.getElementsByClassName("listitem");
+    const listArr = Array.from(listitem);
+    listArr.map((el) => el.classList.remove("active"));
+
+    const dashboradbutton = document.getElementById("allclients");
+    dashboradbutton.classList.add("active");
+
+    history.push("/All-Clients");
+  };
+
   return (
     <div className="sidebar-bg  h-100">
-      <List component="nav" aria-label="main mailbox folders" className="list">
-        {/* <Link
-          className="link-item"
-          to={{
-            pathname: "/estimation",
-            state: { pageName: "Allestimation" },
-          }}
-        > */}
+      <List
+        id="list-id"
+        component="nav"
+        aria-label="main mailbox folders"
+        className="list"
+      >
         <ListItem
           button
           className="listitem"
-          onClick={() => history.push("/Recent-Estimations")}
+          id="dashboard"
+          activeClassName="active"
+          onClick={dashboardClick}
         >
           <ListItemIcon>
             <HomeIcon className="link-icon" />
           </ListItemIcon>
           <span>&nbsp;Dashboard</span>
         </ListItem>
-        {/* </Link> */}
-        {/* <Link className="link-item" to="/allclient"> */}
+
         <ListItem
           button
+          id="allclients"
           className="listitem"
-          onClick={() => history.push("/All-Clients")}
+          activeClassName="active"
+          onClick={clientClick}
         >
           <ListItemIcon>
             <AssignmentIndIcon className="link-icon" />
           </ListItemIcon>
           <span>&nbsp;Clients</span>
         </ListItem>
-        {/* </Link> */}
       </List>
     </div>
   );
