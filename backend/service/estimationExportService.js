bd4f7219fb756b1b1f615f80e2bb63b3ab06b55f
tusharpyramidconsultinginc <93036983+tusharpyramidconsultinginc@users.noreply.github.com>
2022-01-04 17:49:59 +0530

    excel file genrated in report directory

--- backend/service/estimationExportService.js
+++ backend/service/estimationExportService.js
@@ -1,14 +1,61 @@
 const estimationRequirementService = require("./estimationRequirementService");
+const ExcelJS = require("exceljs");
+
+function generateSpreadsheet(colHeader, row) {
+  const workbook = new ExcelJS.Workbook();
+  workbook.creator = "Estimation";
+  workbook.created = new Date();
+
+  const worksheet = workbook.addWorksheet("Estimation Detail");
+
+  worksheet.columns = colHeader;
+
+  worksheet.addRows(row);
+
+  workbook.xlsx.writeFile("./report/Estimation.xlsx");
+}
 
 module.exports.requiredData = async (conditions) => {
-  if (
-    conditions.reports.some(
-      (el) => el.key === "estimationDetail" && el.value === true
-    )
-  ) {
-    const payload = { id: conditions.estimationHeaderId };
-    const requirementData =
-      await estimationRequirementService.getRequirementData(payload);
-    console.log("requirementData");
-  }
+  //estimationDetail
+  const payload = { id: conditions.estimationHeaderId };
+  const requirementData = await estimationRequirementService.getRequirementData(
+    payload
+  );
+  console.log(
+    "requirementData",
+    requirementData.estHeaderAttribute,
+    requirementData.requirementList
+  );
+
+  var estHeaderAttribute = [
+    {
+      header: "Requirement",
+      key: "Requirement",
+      width: 20,
+    },
+    {
+      header: "Tag",
+      key: "Tag",
+      width: 20,
+    },
+    {
+      header: "Description",
+      key: "Description",
+      width: 40,
+    },
+  ];
+
+  estHeaderAttribute = estHeaderAttribute.concat(
+    requirementData.estHeaderAttribute.map((el) => {
+      return { header: el.headerName, key: el.id };
+    })
+  );
+
+  console.log("estHeaderAttribute", estHeaderAttribute);
+
+  const requirementDataArrayList = requirementData.requirementList;
+
+  console.log("requirementDataArrayList", requirementDataArrayList);
+
+  generateSpreadsheet(estHeaderAttribute, requirementDataArrayList);
 };
