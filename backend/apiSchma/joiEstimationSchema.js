446cc8d2caa684d3d7910bf50d497440232ef9de
tusharpyramidconsultinginc <93036983+tusharpyramidconsultinginc@users.noreply.github.com>
2022-01-19 21:17:35 +0530

    task 681: FED : Create a multi selection list to show locations in estimation configuration at first step

--- backend/apiSchma/joiEstimationSchema.js
+++ backend/apiSchma/joiEstimationSchema.js
@@ -1,54 +1,54 @@
 const Joi = require("joi");
 module.exports.createEstimationSchema = Joi.object({
-    estimationName: Joi.string().required(),
-    estimationDescription: Joi.string().required(),
-    estimationType: Joi.string().required(),
-    clientName: Joi.string().required(),
-    projectName: Joi.string().required(),
-    lastupdate: Joi.date().required()
-})
+  estimationName: Joi.string().required(),
+  estimationDescription: Joi.string().required(),
+  estimationType: Joi.string().required(),
+  clientName: Joi.string().required(),
+  projectName: Joi.string().required(),
+  lastupdate: Joi.date().required(),
+});
 module.exports.getallEstimationSchema = Joi.object({
-    skip: Joi.string(),
-    limit: Joi.string()
-})
+  skip: Joi.string(),
+  limit: Joi.string(),
+});
 module.exports.UpdateEstimationHeaderSchema = Joi.object({
-    estheaderParentid: Joi.string().required(),
-    estVersionno: Joi.number().required(),
-    projectId: Joi.string().required(),
-    estName: Joi.string().required(),
-    estTypeId: Joi.string().required(),
-    estDescription: Joi.string().required(),
-    effortUnit: Joi.string().required(),
-    manCount: Joi.number(),
-    contingency: Joi.number().required(),
-    totalCost: Joi.number(),
-    estCalcColumns: Joi.string(),
-    estColumns: Joi.string(),
-    isDeleted: Joi.boolean(),
-    createdBy: Joi.string(),
-    updatedBy: Joi.string(),
-    estStep : Joi.string(),
-    estTentativeTimeline: Joi.number().required(),
-})
-
-
+  estheaderParentid: Joi.string().required(),
+  estVersionno: Joi.number().required(),
+  projectId: Joi.string().required(),
+  estName: Joi.string().required(),
+  estTypeId: Joi.string().required(),
+  estDescription: Joi.string().required(),
+  effortUnit: Joi.string().required(),
+  manCount: Joi.number(),
+  contingency: Joi.number().required(),
+  totalCost: Joi.number(),
+  estCalcColumns: Joi.string(),
+  estColumns: Joi.string(),
+  isDeleted: Joi.boolean(),
+  createdBy: Joi.string(),
+  updatedBy: Joi.string(),
+  estStep: Joi.string(),
+  estTentativeTimeline: Joi.number().required(),
+  locations: Joi.array().required(),
+});
 
 module.exports.createEstimationHeaderSchema = Joi.object({
-    estheaderParentid: Joi.string().required(),
-    estVersionno: Joi.number().required(),
-    projectId: Joi.string().required(),
-    estName: Joi.string().required(),
-    estTypeId: Joi.string().required(),
-    estDescription: Joi.string(),
-    effortUnit: Joi.string(),
-    manCount: Joi.number(),
-    contingency: Joi.number().required(),
-    totalCost: Joi.number(),
-    estCalcColumns: Joi.string(),
-    estColumns: Joi.string(),
-    isDeleted: Joi.boolean(),
-    createdBy: Joi.string(),
-    updatedBy: Joi.string(),
-    estStep : Joi.string(),
-    estTentativeTimeline: Joi.number().required()
-})
+  estheaderParentid: Joi.string().required(),
+  estVersionno: Joi.number().required(),
+  projectId: Joi.string().required(),
+  estName: Joi.string().required(),
+  estTypeId: Joi.string().required(),
+  estDescription: Joi.string(),
+  effortUnit: Joi.string(),
+  manCount: Joi.number(),
+  contingency: Joi.number().required(),
+  totalCost: Joi.number(),
+  estCalcColumns: Joi.string(),
+  estColumns: Joi.string(),
+  isDeleted: Joi.boolean(),
+  createdBy: Joi.string(),
+  updatedBy: Joi.string(),
+  estStep: Joi.string(),
+  estTentativeTimeline: Joi.number().required(),
+  locations: Joi.array().required(),
+});
